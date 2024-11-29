af6ae9956339da8326d22fb4c49caa1875cafe72
sudhanshu mehto <sudhanshumehto693@gmail.com>
2024-11-29 12:39:09 +0530

    third commit

--- november29/script.js
+++ november29/script.js
@@ -1,6 +1,23 @@
-function change(){
+var name1 = document.getElementById("demo1");
+var namee = document.getElementById("demo2");
+var nameee = document.getElementById("demo3");
+function name(){
+    name1.innerHTML = "sudhanshu";
+    alert("sudhanshu");
+    console.log("sudhanshu");
+}
+name();
 
-    var heading = document.getElementById("demo");
-    heading.innerHTML = "button was clicked and text changed";
-    heading.style.color = "red";
-    }
+const name2 = function(){
+    namee.innerHTML = "Shiva";
+    alert("shiva");
+    console.log("shiva");
+}
+name2();
+
+var name3 = () =>{
+    nameee.innerHTML = "abhinav";
+    alert("abhinav");
+    console.log("abhinav");
+}
+name3();
