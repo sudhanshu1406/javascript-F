6e13ec44b8d0796814b5e26743a6c1e13aec063b
sudhanshu mehto <sudhanshumehto693@gmail.com>
2024-11-30 12:44:57 +0530

    30 november

---
+++ november30/script.js
@@ -0,0 +1,23 @@
+var name = "sudhanshu";
+var city = "bhopal";
+var lstname = "mehto";
+var data = `this is our first string with backtick`;
+
+
+// template literals or string literals
+
+var info = document.getElementById("demo").innerHTML =`this is magic (${data} ${name} ${lstname})`;
+var div = document.getElementById("trial");
+// div.innerHTML = "<h1>" + name +"</h1>";
+div.innerHTML = `<h1>${data}</h1>`
+
+// write a function to display the name and city of a person using template literals and display it in innerhtml
+
+var heading = document.getElementById("new");
+heading.addEventListener("click")
+
+function show(){
+    heading.innerHTML = `${name} ${city}`;
+}
+
+
